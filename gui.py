f01b88de84636efd295d88cb3f46673be3897184
timoti1 <timoti@tut.by>
2019-03-17 16:27:05 +0300

    tkinter gui

---
+++ gui.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+def onObjectClick(event):
+    print('Clicked', event.x, event.y, event.widget)
+    print(event.widget.find_closest(event.x, event.y))
+
+root = Tk()
+canv = Canvas(root, width=100, height=100)
+obj1 = canv.create_text(50, 30, text='Click me one')
+obj2 = canv.create_text(50, 70, text='Click me two')
+
+canv.tag_bind(obj1, '<Button-1>', onObjectClick)
+canv.tag_bind(obj2, '<Button-1>', onObjectClick)
+canv.pack()
+
+root.mainloop()
